f8e2f07f5adc0834d20e39ee8e9a75492e1a7761
André Perna <andreperna@gmail.com>
2021-05-24 15:12:59 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//---------- ROTAS
+const handleRoot = (req, res) => {
+  res.send("Teste IBM");
+};
+app.get("/", handleRoot);
+
+//---------- INICIA SERVIDOR
+const handleServerListen = () => console.log(`Servidor rodando na porta: ${port}`);
+app.listen(port, handleServerListen);
